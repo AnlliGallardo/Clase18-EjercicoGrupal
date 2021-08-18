f7f4a1749c1a59b639e29161cb05de64dd89d114
Luis Corrales Marin <luiscorrales5012@gmail.com>
2021-08-18 13:00:16 -0500

    Luis

---
+++ script/index.js
@@ -0,0 +1,1 @@
+import Pais from "./modulos/mOrdenamiento";
---
+++ script/modulos/mOrdenamiento.js
@@ -0,0 +1,10 @@
+export default Pais; 
+
+class Paises{
+    constructor(nombre_pais){
+        this.numero_clasificacion = 0;
+        this.nombre = nombre_pais;
+        this.distancia_paises = 0;
+        this.tamanio = 0;
+    }
+}
